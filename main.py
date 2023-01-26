603ef0db0cab3c9be23166c96384fc40fcb1595d
Jeff Vestal <53237856+jeffvestal@users.noreply.github.com>
2023-01-26 19:42:13 +0000

    getting _stats and fixing the reporting formatting

--- main.py
+++ main.py
@@ -18,20 +18,27 @@ def esConnect(cid, user, passwd):
     
     return es
 
-def update_model_settings(es, model_id, at, cache='0b'):
+def update_model_settings(es, model_id, at, cache='0b', retry_count=0):
     '''update the model allocation and threads per allocation'''
 
     # Don't have to stop when only updating allocation but its quick enough to do it everytime for now
     stop = MlClient.stop_trained_model_deployment(es, model_id=model_id, force=True)
     
-    start = MlClient.start_trained_model_deployment(es, 
+    try:
+        start = MlClient.start_trained_model_deployment(es, 
                                                     model_id=model_id, 
                                                     cache_size = cache,
                                                     number_of_allocations = at[0],
                                                     threads_per_allocation = at[1],
                                                     wait_for='started',
-                                                    timeout='7m'
+                                                    timeout='10m'
                                                    )
+    #except elastic_transport.ConnectionTimeout:
+    except:
+        if retry_count < 2:
+            update_model_settings(es, model_id, at, cache, retry_count=1)
+        else:
+            raise            
 
     return
 
@@ -50,7 +57,7 @@ def create_new_index(es, index_name):
 
     # may want to set the mapping
 
-    print(index_name)
+    #print(index_name)
     
     response = es.indices.create(index=index_name, body=index_settings)
     if not response['acknowledged']:
@@ -62,7 +69,12 @@ def create_new_index(es, index_name):
 
 
 def start_reindex(es, source, destination, pipe):
-    '''kick off _reindex'''
+    '''
+    kick off _reindex after 60 second pause
+    This is to allow the _last_minute metrics to 0 out
+    '''
+
+    time.sleep(60)
 
     request_body = {
         "source": {
@@ -79,30 +91,72 @@ def start_reindex(es, source, destination, pipe):
 
     return response
 
-def wait_until_ingest_complete(es, response, wait=5):
+
+def get_inference_throughput(es, model):
+    ''' 
+    call _stats to get average_inference_time_ms_last_minute` and `throughput_last_minute`
+    This pulls the max accross all nodes
+    TODO track max per node
+    '''
+
+    i, t = [], []
+    response = MlClient.get_trained_models_stats(es, model_id=model)
+    print(response)
+    for m in response['trained_model_stats']:
+        for n in m['deployment_stats']['nodes']:
+            try:
+                i.append(n['average_inference_time_ms_last_minute'])
+            except KeyError:
+                i.append(0)
+            try:
+                t.append(n['throughput_last_minute'])
+            except KeyError:
+                t.append(0)
+
+    return(max(i), max(t))
+    
+    
+def wait_until_ingest_complete(es, response, model, wait=5):
     '''
     poll every 'wait' seconds, return when reindex thread is complete
     default wait = 10 seconds
 
+    Also poll _ml/trained_models/_stats to get `average_inference_time_ms_last_minute` and `throughput_last_minute`
     '''
+
+    inference, throughput = [], []
+    
     while True:
         task_info = es.tasks.get(task_id=response['task'])
         if task_info['completed']:
-            #print("Task completed!")
+            it = get_inference_throughput(es, model)
+            inference.append(it[0])
+            throughput.append(it)
             break
         else:
-            #print("Task not completed, waiting...")
+            it = get_inference_throughput(es, model)
+            inference.append(it[0])
+            throughput.append(it)
             time.sleep(wait)
 
-    print(task_info)
-    return(task_info['task']['running_time_in_nanos'])
+
+    inference, throughput = max(inference), max(throughput)
+
+    #print(task_info)
+    return(task_info['task']['running_time_in_nanos'], inference, throughput)
 
 
 if __name__ == '__main__':
 
     # file output results name
     results = 'pipeline-results__' + str(datetime.now())
-    tmpResults = []
+    tmpResults = [(
+        'allocation x threads per allocation',
+        '_reindex running_time_in_nanos in seconds',
+        '_reindex running_time_in_nanos',
+        'max average_inference_time_ms_last_minute',
+        'max throughput_last_minute'
+    )]
     
     # setup elastic cloud connection
     es_cloud_id = os.environ['es_cloud_id']
@@ -112,21 +166,22 @@ if __name__ == '__main__':
     
     # set the test pairs of allocations and threads per allocation on the supervised model
     allocation_threadsPer = [
-        [1,8],
-        [1,1],
-        [4,1],
+        #[1,8],
+        #[1,1],
+        #[4,1],
         [8,1],
-        [1,4],
-        [2,8],
-        [4,4],
+        #[1,4],
+        #[2,8],
+        #[4,4],
         [16,1]
     ]
 
     # set source index name
     sourceIndex = 'pii_test-no_redaction'
     
-    # set the base name of the test index set - reindexName__pipelineName__AxT
+    # set the base name of the test index set - reindexName__pipelineName__runTS__AxT
     reindexName = 'pii_test_v2'
+    runTS = datetime.now().strftime('%Y%m%d_%H%M%S')
 
     # set the ingest pipeline to use
     pipelineName = 'pii_script-redact-throuput_test_01'
@@ -142,32 +197,43 @@ if __name__ == '__main__':
 
     # Make go now
     for at in allocation_threadsPer:
-        print(at)
+        #print(at)
         configString = 'x'.join(map(str, at))
+        #print(configString)
         
         # create new index name
         indexName = '__'.join([reindexName, 
-                               pipelineName, 
+                               pipelineName,
+                               runTS,
                                configString
                               ])
 
-        tmpResults.append(indexName)
+        #tmpResults.append(indexName)
         # create new index
         create_new_index(es,indexName)
+        
 
         # update supervised model with this round's settings
         update_model_settings(es, modelID, at)
         
         # kickoff _reindex
         reindexResponse = start_reindex(es, sourceIndex, indexName, pipelineName)
-        print(reindexResponse)
+        #print(reindexResponse)
 
         # wait for _reindex to complete
-        elapsed_time = wait_until_ingest_complete(es, reindexResponse)
-        tmp = configString, ": ", elapsed_time / 1000000000, "seconds (", elapsed_time, " nanos)"
+        elapsed_time, inf_max, tp_max = wait_until_ingest_complete(es, reindexResponse, modelID)
+        print(inf_max)
+        print(tp_max)
+        
+        #tmp = configString, ": ", elapsed_time / 1000000000, "seconds (", elapsed_time, " nanos), max thro"
+        tmp = ','.join(map(str, (configString, elapsed_time / 1000000000, elapsed_time,  inf_max, tp_max)))
         tmpResults.append(tmp)
         print(tmp)
-        
+
+    print('Done with configuration options')
+    print(tmpResults)
     with open(results, "w") as file:
-        file.writelines('\n'.join([''.join(map(str, x)) for x in tmpResults]))    
+        #file.writelines('\n'.join([''.join(map(str, x)) for x in tmpResults]))    
+        file.writelines('\n'.join(tmpResults))    
+        
     print('Done')
