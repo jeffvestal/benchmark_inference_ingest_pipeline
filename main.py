8b481f1c4aaa21ed0abc4bc042bb368c3cdb8747
Jeff Vestal <53237856+jeffvestal@users.noreply.github.com>
2023-01-26 03:02:03 +0000

    saving initial code, not ready yet

---
+++ main.py
@@ -0,0 +1,144 @@
+'''
+test inference processor pipeline using _reindex
+and different allocation and threads per allocations settings
+'''
+
+from elasticsearch import Elasticsearch, helpers
+from elasticsearch.client import MlClient
+import os
+import timeit
+
+
+def esConnect(cid, user, passwd):
+    '''Connect to Elastic Cloud cluster'''
+
+    #TODO switch to API key?
+    es = Elasticsearch(cloud_id=cid, http_auth=(user, passwd))
+    
+    return es
+
+def update_model_settings(es, model_id, cache, at):
+    '''update the model allocation and threads per allocation'''
+
+    # Don't have to stop when only updating allocation but its quick enough to do it everytime for now
+    stop = MlClient.stop_trained_model_deployment(es, model_id=model_id)
+    
+    start = MlClient.start_trained_model_deployment(es, 
+                                                    model_id=model_id, 
+                                                    cache_size = '0b',
+                                                    number_of_allocations = at[0],
+                                                    threads_per_allocation = at[1],
+                                                    wait_for=started,
+                                                    timeout=1m
+                                                   )
+
+    return
+
+
+def create_new_index(es, index_name):
+    '''
+    Create the new index before reindex is started
+    '''
+
+    index_settings = {
+        "settings": {
+            "number_of_shards": 1,
+            "number_of_replicas": 1
+        }
+    }
+
+    # may want to set the mapping
+    
+    response = es.indices.create(index=index_name, body=index_settings)
+    status_code = response.status_code
+    if status_code != 200:
+        response_content = response.json()
+        print(response_content)
+        sys.exit()
+    else:
+        return 
+
+
+def start_reindex(es, source, destination):
+    '''kick off _reindex'''
+
+    request_body = {
+        "source": {
+            "index": source
+        },
+        "dest": {
+            "index": destination
+        }
+    }
+    
+    # Execute the reindex operation
+    thread = es.reindex(body=request_body)
+    
+
+
+def wait_until_ingest_complete(threadID, wait=10):
+    '''
+    poll every 'wait' seconds, return when reindex thread is complete
+    default wait = 10 seconds
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    
+    # setup elastic cloud connection
+    es_cloud_id = os.environ('es_cloud_id')
+    es_cloud_user = os.environ('es_cloud_user')
+    es_cloud_pass = os.environ('es_cloud_pass')
+
+    
+    # set the test pairs of allocations and threads per allocation on the supervised model
+    allocation_threadsPer = [
+        [1,1],
+        [4,1],
+        [8,1],
+        [1,4],
+        [1,8],
+        [2,8],
+        [4,4],
+        [16,1]
+    ]
+
+    # set source index name
+    sourceIndex = ''
+    
+    # set the base name of the test index set - reindexName__pipelineName__AxT
+    reindexName = 'pii_test_v2'
+
+    # set the ingest pipeline to use
+    pipelineName = 'pii_script-redact-throuput_test_01'
+
+    # set the name of the supervised model_id used in the pipeline
+    # TODO could be smarter and get this from the pipeline config automatically
+    modelID = 'dslim__bert-base-ner'
+    
+    #############
+    # create elastic connection
+    # TODO switch to apikey
+    es = esConnect(es_cloud_id, es_cloud_user, es_cloud_pass)
+
+    # Make go now
+    for at in allocation_threadsPer:
+        configString = 'x'.join(map(str, at)
+        # create new index name
+        indexName = '__'.join([reindexName, 
+                               pipelineName, 
+                               configString)
+                              ])
+
+        # create new index
+        create_new_index(es,indexName)
+
+        # update supervised model with this round's settings
+        update_model_settings(es, modelID, at)
+        
+        # kickoff _reindex
+        elapsed_time = timeit.timeit(start_reindex(), number=1)
+        print(configString, ": ", elapsed_time, "seconds")
+
+    
